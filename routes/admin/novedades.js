6462962174dd10a73de36669d169f1f46964ed29
Angie <diazortigoza.mariangel@gmail.com>
2022-08-05 18:14:04 -0300

    conexión sección novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
 
-router.get('/', function(req, res, next) {
+/*listar novedades*/
+
+router.get('/', async function(req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
     res.render('admin/novedades', { 
         layout:'admin/layout',
-        persona: req.session.nombre
+        persona: req.session.nombre,
+        novedades
     });
   });
+
+/*eliminar novedades*/ 
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+});
 
   
 
