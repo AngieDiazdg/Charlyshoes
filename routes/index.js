f5e33b7edb24acb5577d05569dafb078599ad970
Angie <diazortigoza.mariangel@gmail.com>
2022-08-22 14:16:11 -0300

    img

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;  
 
 
 
@@ -9,6 +10,26 @@ var novedadesModel = require('../models/novedadesModel');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.splice(0,5); // seleccionamos los primeros 5 del array
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+    return {
+      ...novedad,
+      imagen
+    }
+  } else {
+    return {
+      ...novedad,
+      imagen: '/images/noimage.jpg'
+    }
+  }
+});
+
   res.render('index', {
     novedades
   });
